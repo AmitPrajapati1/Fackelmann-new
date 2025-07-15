73a123b37e45de68f78e2492f76284e9f9ab54c1
Developer0303 <Developer0303@users.noreply.github.com>
2025-07-15 11:17:16 +0530

    added submenus and updated sub categories, fixed searbar issue

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Cookware Carousel</title>
+  <title>Fackelmann</title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
